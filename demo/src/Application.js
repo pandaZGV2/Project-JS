a78d0f686536ef648142446f26ff42952b0b7496
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-26 20:27:38 +0400

    Checking Counts

--- demo/src/Application.js
+++ demo/src/Application.js
@@ -6,7 +6,8 @@ class Application extends Component {
     constructor(props) {
         super(props);// Instantiates Parent Class not everytime its re-rendered
         this.state = {
-            count: 0
+            count: 0,
+            overTen: false
         }
     };
     // componentWillMount(props, state) {
@@ -24,19 +25,26 @@ class Application extends Component {
     //         //Do something
     //     }
     // }
-    
+
     handleClick = () => {
         this.setState({ count: this.state.count + 1 });
 
     }
     componentDidUpdate(props, state) {
-        this.setState({count: this.state.count  + 1});//Doesnt work as it repeatedly triggers componentDidUpdate so you get Maximum Depth exceeded error.
+        if (this.state.count > 10) {
+            this.setState({ overTen: true });
+        }
+        //this.setState({count: this.state.count  + 1});//Doesnt work as it repeatedly triggers componentDidUpdate so you get Maximum Depth exceeded error.
     }
     render() {
         let name = "Panda";
         let { count } = this.state;
         return (<div>
             <h1>You clicked the button {count} times.</h1>
+            {(this.state.overTen && this.state.count != state.count) ?
+                <h1>You have crossed 10</h1>
+                : null
+            }
             <span><button onClick={(e) => this.handleClick()}>Click Me!</button></span>
         </div>
         );
