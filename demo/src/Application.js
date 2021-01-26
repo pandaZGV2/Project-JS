d13168fe876c6a6172975be12a4b28300694e1fe
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-26 19:56:46 +0400

    bacc to work

---
+++ demo/src/Application.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line
+import React, { Component } from 'react';
+// eslint-disable-next-line
+
+class Application extends Component {
+    render() {
+        let name = "Panda";
+        return (<div>
+            <h1>Hello, {name}</h1>
+            <span>Hello there Mate</span>
+        </div>
+        );
+    }
+
+
+}
+
+export default Application
