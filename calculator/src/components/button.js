71a119ab59e70c3c670f7a8314a819c81e8a83bd
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-28 14:36:16 +0400

    Button design and ui

--- calculator/src/components/button.js
+++ calculator/src/components/button.js
@@ -1,12 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class Button extends Component{
-    render()
-    {
-        return(
-            <div>
-                <button class="calc-button">1</button>
-            </div>
+class Button extends Component {
+    render() {
+        return (
+            <button className={`column-${this.props.cols}`} class="calc-button" onClick={(e) => this.props.action(this.props.symbol)}>{this.props.symbol}</button>
         )
     }
 
