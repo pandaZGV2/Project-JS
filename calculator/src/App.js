f8cca89c3d7a10caf7761b56d47f5b35a1a6390e
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-28 16:05:59 +0400

    Debugging stuff

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -20,52 +20,59 @@ class App extends Component {
   }
   addtoCurrent = (symbol) => {
     console.log(symbol)
-    if(["/","X","+","-"].indexOf(symbol)>-1)
-    {
-      let {previous} = this.state;
-      previous.concat(this.state.current + symbol);
-      this.state.previous({previous})//keyname vale is stored in this.state.previous
+    if (["/", "X", "+", "-"].indexOf(symbol) > -1) {
+      let { previous } = this.state;
+      previous.push(this.state.current + symbol);
+      this.state.previous({ previous })//keyname value is stored in this.state.previous
     }
-    this.setState({ current: this.state.current + symbol });
+    else {
+      if (this.state.current === "0" && symbol !== ".") {
+        this.setState({ current: symbol })
+      }
+
+      else {
+        this.setState({ current: this.state.current + symbol });
+      }
+    }
   }
 
-  render() {
-    const buttons = [
-      { symbol: 'C', cols: 3, action: this.reset },
-      { symbol: '/', cols: 1, action: this.addtoCurrent },
+render() {
+  const buttons = [
+    { symbol: 'C', cols: 3, action: this.reset },
+    { symbol: '/', cols: 1, action: this.addtoCurrent },
 
-      { symbol: '7', cols: 1, action: this.addtoCurrent },
-      { symbol: '8', cols: 1, action: this.addtoCurrent },
-      { symbol: '9', cols: 1, action: this.addtoCurrent },
-      { symbol: 'X', cols: 1, action: this.addtoCurrent },
+    { symbol: '7', cols: 1, action: this.addtoCurrent },
+    { symbol: '8', cols: 1, action: this.addtoCurrent },
+    { symbol: '9', cols: 1, action: this.addtoCurrent },
+    { symbol: 'X', cols: 1, action: this.addtoCurrent },
 
-      { symbol: '4', cols: 1, action: this.addtoCurrent },
-      { symbol: '5', cols: 1, action: this.addtoCurrent },
-      { symbol: '6', cols: 1, action: this.addtoCurrent },
-      { symbol: '-', cols: 1, action: this.addtoCurrent },
+    { symbol: '4', cols: 1, action: this.addtoCurrent },
+    { symbol: '5', cols: 1, action: this.addtoCurrent },
+    { symbol: '6', cols: 1, action: this.addtoCurrent },
+    { symbol: '-', cols: 1, action: this.addtoCurrent },
 
-      { symbol: '1', cols: 1, action: this.addtoCurrent },
-      { symbol: '2', cols: 1, action: this.addtoCurrent },
-      { symbol: '3', cols: 1, action: this.addtoCurrent },
-      { symbol: '+', cols: 1, action: this.addtoCurrent },
+    { symbol: '1', cols: 1, action: this.addtoCurrent },
+    { symbol: '2', cols: 1, action: this.addtoCurrent },
+    { symbol: '3', cols: 1, action: this.addtoCurrent },
+    { symbol: '+', cols: 1, action: this.addtoCurrent },
 
-      { symbol: '0', cols: 1, action: this.addtoCurrent },
-      { symbol: '.', cols: 1, action: this.addtoCurrent },
-      { symbol: '=', cols: 2, action: this.addtoCurrent }
-    ];
-    return (
-      <div className="App">
-        <input value={this.state.current} className="result" type="text" value={this.state.current} />
-        <div>{
-          buttons.map((btn, i) => {
-            return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)} />
+    { symbol: '0', cols: 1, action: this.addtoCurrent },
+    { symbol: '.', cols: 1, action: this.addtoCurrent },
+    { symbol: '=', cols: 2, action: this.addtoCurrent }
+  ];
+  return (
+    <div className="App">
+      <input value={this.state.current} className="result" type="text" value={this.state.current} />
+      <div>{
+        buttons.map((btn, i) => {
+          return <Button key={i} symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action(symbol)} />
 
-          })
-        }
-        </div>
+        })
+      }
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default App;
